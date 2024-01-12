bf963dbda71849889759bffed99ed555ed8e3173
valhallajazzy <valhallajazzy@mail.ru>
2024-01-13 03:25:59 +0600

    Refactor main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import os
-from itertools import chain
 
 from dotenv import load_dotenv
 import requests
@@ -9,13 +8,13 @@ LANGUAGES = ['JavaScript', 'Java', 'Python', 'Ruby', 'PHP', 'C++', 'Go', 'Swift'
 
 
 def get_average_salary(salary_from, salary_to):
-    if salary_from and salary_to != 0:
+    if salary_from and salary_to:
         return (salary_from + salary_to) / 2
-    if salary_from == 0 and salary_to != 0:
+    if not salary_from and salary_to:
         return salary_to * 0.8
-    if salary_from != 0 and salary_to == 0:
+    if salary_from and not salary_to:
         return salary_from * 1.2
-    if salary_from == 0 and salary_to == 0:
+    if not salary_from and salary_to:
         return None
 
 
@@ -38,14 +37,12 @@ def get_all_vacancies_by_prog_language_hh(language):
     return all_pages_by_request
 
 
-def predict_rub_salaries_and_count_all_vacancies_hh(language):
-    all_pages_with_vacancies = get_all_vacancies_by_prog_language_hh(language)
-    found_vacancies = all_pages_with_vacancies[0]['found']
+def predict_rub_salaries_hh(all_pages_with_vacancies):
     middle_salaries = []
     for page in all_pages_with_vacancies:
         vacancies = page['items']
-        salaries = [vacancy['salary'] for vacancy in vacancies]
-        for salary in salaries:
+        for vacancy in vacancies:
+            salary = vacancy['salary']
             if salary:
                 if salary['currency'] != 'RUR':
                     continue
@@ -54,97 +51,81 @@ def predict_rub_salaries_and_count_all_vacancies_hh(language):
                 if not salary['to']:
                     salary['to'] = 0
                 average_salary = get_average_salary(salary['from'], salary['to'])
-                if average_salary is not None:
+                if average_salary:
                     middle_salaries.append(average_salary)
-                else:
-                    continue
             else:
                 continue
-    return middle_salaries, found_vacancies
+    return middle_salaries
 
 
 def get_job_statistics_hh(languages=LANGUAGES):
     processed_statistics = {}
     for language in languages:
-        rub_salaries_and_vacancies_found = predict_rub_salaries_and_count_all_vacancies_hh(language)
-        if len(rub_salaries_and_vacancies_found[0]) == 0:
+        all_pages_with_vacancies_hh = get_all_vacancies_by_prog_language_hh(language)
+        predict_rub_salaries = predict_rub_salaries_hh(all_pages_with_vacancies_hh)
+        if not len(predict_rub_salaries):
             len_salaries = 1
         else:
-            len_salaries = len(rub_salaries_and_vacancies_found[0])
+            len_salaries = len(predict_rub_salaries)
         processed_language = {
-            "vacancies_found": rub_salaries_and_vacancies_found[1],
-            "vacancies_processed": len(rub_salaries_and_vacancies_found[0]),
-            "average_salary": int(sum(rub_salaries_and_vacancies_found[0]) / len_salaries)
+            "vacancies_found": all_pages_with_vacancies_hh[0]['found'],
+            "vacancies_processed": len(predict_rub_salaries),
+            "average_salary": int(sum(predict_rub_salaries) / len_salaries)
         }
         processed_statistics[language] = processed_language
     return processed_statistics
 
 
-def get_all_vacancies_by_industry_sj(token):
+def get_all_vacancies_by_prog_language_sj(token, language):
     all_pages_by_request = []
-    count_of_vacancies = 1
-    verified_count_of_vacancies = 0
+    more_results = True
     page = 0
-    while verified_count_of_vacancies < count_of_vacancies:
+    while more_results:
         url = 'https://api.superjob.ru/2.0/vacancies/'
         catalog_number_by_industry = 48
         headers = {
             'X-Api-App-Id': token}
         params = {'catalogues': catalog_number_by_industry,
                   'town': 'Москва',
-                  'page': page}
+                  'page': page,
+                  'keyword': language}
         response = requests.get(url, headers=headers, params=params)
         response.raise_for_status()
-        response_objects = response.json()['objects']
-        all_pages_by_request.append(response_objects)
+        vacancies = response.json()
+        all_pages_by_request.append(vacancies)
         page += 1
-        verified_count_of_vacancies += len(response_objects)
-        if count_of_vacancies == 1:
-            count_of_vacancies = response.json()['total']
-    all_pages_by_request = list(chain.from_iterable(all_pages_by_request))
+        more_results = response.json()['more']
     return all_pages_by_request
 
 
-def predict_rub_salary_sj(token, vacancy):
-    vacancies = get_all_vacancies_by_industry_sj(token)
+def predict_rub_salary_sj(all_pages_with_vacancies_sj):
     middle_salaries = []
-    for doc in vacancies:
-        split_phrase = doc['profession'].split()
-        if vacancy in split_phrase:
-            if doc['currency'] == 'rub':
-                average_salary = get_average_salary(doc['payment_from'], doc['payment_to'])
-                if average_salary is not None:
+    for page in all_pages_with_vacancies_sj:
+        vacancies = page['objects']
+        for vacancy in vacancies:
+            if vacancy['currency'] == 'rub':
+                average_salary = get_average_salary(vacancy['payment_from'], vacancy['payment_to'])
+                if average_salary:
                     middle_salaries.append(average_salary)
-                else:
-                    continue
             else:
                 continue
     return middle_salaries
 
 
-def count_vacancies_by_request_sj(token, vacancy):
-    vacancies = get_all_vacancies_by_industry_sj(token)
-    vacancies_found = 0
-    for doc in vacancies:
-        split_phrase = doc['profession'].split()
-        if vacancy in split_phrase:
-            vacancies_found += 1
-    return vacancies_found
-
-
 def get_job_statistics_sj(token, languages=LANGUAGES):
     processed_statistics = {}
     for language in languages:
-        count_vacancies = count_vacancies_by_request_sj(token, language)
-        salaries = predict_rub_salary_sj(token, language)
-        if len(salaries) == 0:
+        all_pages_with_vacancies_sj = get_all_vacancies_by_prog_language_sj(token, language)
+        count_vacancies = all_pages_with_vacancies_sj[0]['total']
+        salaries = predict_rub_salary_sj(all_pages_with_vacancies_sj)
+        if not len(salaries):
             len_salaries = 1
         else:
             len_salaries = len(salaries)
 
         processed_language = {
             "vacancies_found": count_vacancies,
-            "vacancies_processed": count_vacancies,
+            "vacancies_processed": len_salaries,
             "average_salary": int(sum(salaries) / len_salaries)
         }
         processed_statistics[language] = processed_language
@@ -152,15 +133,14 @@ def get_job_statistics_sj(token, languages=LANGUAGES):
 
 
 def create_table(statistic, header, languages=LANGUAGES):
-    table_data = [[languages[n], statistic[languages[n]]['vacancies_found'],
+    table_columns = [[languages[n], statistic[languages[n]]['vacancies_found'],
                    statistic[languages[n]]['vacancies_processed'],
                    statistic[languages[n]]['average_salary']] for n in range(len(languages))]
     table = [
         ['Язык программирования', 'Вакансий найдено', 'Вакансий обработано', 'Средняя зарплата'],
-        *table_data
+        *table_columns
         ]
-    table = AsciiTable(table, header)
-    print(table.table)
+    return AsciiTable(table, header)
 
 
 def main():
@@ -168,9 +148,11 @@ def main():
     token = os.environ["TOKEN_SUPER_JOB"]
     hh_statistic = get_job_statistics_hh()
     sj_statistic = get_job_statistics_sj(token)
-    create_table(hh_statistic, 'HeadHunter Moscow')
+    table_hh = create_table(hh_statistic, 'HeadHunter Moscow')
+    print(table_hh.table)
     print()
-    create_table(sj_statistic, 'SuperJob Moscow')
+    table_sj = create_table(sj_statistic, 'SuperJob Moscow')
+    print(table_sj.table)
 
 
 if __name__ == '__main__':
